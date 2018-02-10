974bf662dd2946bfbc1905a92184c94e5b7dd2c7
Artemis <zer00aeon@gmail.com>
2018-02-10 14:48:51 +0200

    added normal waits , last account selection and assertion

--- chrorus_int.py
+++ chrorus_int.py
@@ -1,6 +1,11 @@
+import time
+from threading import Thread
+
+from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
 
 # constants
 from selenium.webdriver.support import wait, ui
@@ -11,14 +16,16 @@ WIN_PATH = "H:\Webdrivers\chromedriver_win32\chromedriver.exe"
 CREEDENTIALS = ["automation@gmail.com", "rhrDBWT78iwU"]
 USER_ACC_URL = "https://chorus.ai/blueprint/205426"
 DRIVER = webdriver.WebDriver(WIN_PATH)
+ACTIONS = ActionChains(DRIVER)
 ACCOUNT_NAME = "Automation.com"
 
 
 class ChorusHW:
 
-    def __init__(self, driver):
+    def __init__(self, driver, actions):
         self.driver = driver
         self.url = 'https://www.chorus.ai/login'
+        self.actions = actions
 
     def navigate_to_login_page(self):
         self.driver.maximize_window()
@@ -48,22 +55,39 @@ class ChorusHW:
 
     def click_on_account(self, account_name):
         # self.driver.find_element_by_partial_link_text(account_name).click()
-        self.driver.find_element_by_xpath("//*[contains(text(),'Automation.com')]").click()
         # self.driver.find_element_by_xpath("//*[contains(text(),"+account_name+"')]").click()
+        self.driver.find_element_by_xpath("//*[contains(text(),'Automation.com')]").click()
 
     def click_hide_under_10_calls(self):
         chorus_checkbox = WebDriverWait(self.driver, 10).until(
             lambda x: x.find_element_by_xpath("//div[contains(@class,'chorus-checkbox')]"))
         chorus_checkbox.click()
 
+    def search_transcripts(self, search_string):
+        search_box = self.driver.find_element_by_xpath("//input[@name='search']")
+        # search_box = self.driver.find_element_by_xpath(
+        #     "//div/form/input")
+        # search_box clicking creates a selenium exception , so in order to save time used action chains
+        # actions = ActionChains(self.driver)
+
+        AS = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.XPATH, "//div/h4[contains(string(), ' 57 - Automation Standup')]")))
+        self.actions.move_to_element(search_box)
+        self.actions.click()
+        self.actions.perform()
+        search_box.send_keys(search_string)
+        search_box.send_keys(Keys.RETURN)
+        AS.click()
+
 
 if __name__ == "__main__":
-    chorus = ChorusHW(DRIVER)
+    chorus = ChorusHW(DRIVER, ACTIONS)
     chorus.navigate_to_login_page()
     chorus.login_into_chorus(CREEDENTIALS, USER_ACC_URL)
     chorus.implicit_wait(5)
     chorus.click_on_account(ACCOUNT_NAME)
-    chorus.implicit_wait(5)
-    chorus.click_hide_under_10_calls()
+    chorus.search_transcripts("bob")
+
+    # chorus.assert_search_results()
 
     # chorus.quit()
